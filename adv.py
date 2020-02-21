ceb3dff38c876b106d92d5ee50cab6e37bf8d8ab
[Brandon] <desselle.brandon2@gmail.com>
2020-02-21 11:33:57 -0600

    Update adv.py

--- adv.py
+++ adv.py
@@ -1,7 +1,8 @@
 from room import Room
 from player import Player
 from world import World
-
+from graph import Graph
+from util import Stack
 import random
 from ast import literal_eval
 
@@ -17,7 +18,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -27,9 +28,55 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
 
 
+def build_path(graph):
+    """
+    Traverse maze in a DFT.
+    """
+
+    # stack that contains our current path
+    s = Stack()
+    # array that contains our returned path
+    moves = []
+    # helps us determine if we hit a dead end
+    visited = set()
+    # initialize traversal with the 0 index room
+    s.push(0)
+
+    while len(visited) < len(graph):
+        # get the id of the current room in the stack
+        id = s.tail()
+        # mark as visited
+        visited.add(id)
+        # get information on the current room (tuple data)
+        current_room = graph[id]
+        # dict of possible moves
+        rooms_dict = current_room[1]
+        # array to track if a room has not been visited yet
+        undiscovered = []
+        # store undiscovered rooms in relationship to the current room
+        for direction, room_id in rooms_dict.items():
+            if room_id not in visited:
+                undiscovered.append(room_id)
+        # assign the next room
+        # if we reached a dead end, back track
+        if len(undiscovered) > 0:
+            next_room = undiscovered[0]
+            s.push(next_room)
+        else:
+            s.pop()
+            next_room = s.tail()
+
+        # survey the rooms around our current room. if the next move matches the room_id, add that to moves and walk
+        for direction, adjacent_id in rooms_dict.items():
+            if adjacent_id == next_room:
+                moves.append(direction)
+
+    return moves
+
+
+traversal_path = build_path(room_graph)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -41,22 +88,23 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# breakpoint()
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
